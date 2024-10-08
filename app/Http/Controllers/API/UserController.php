3b984ccb1075479091a6c81fbffc68ef29f10e6f
SahbiRom <sahbiromdhani666@gmail.com>
2024-10-08 20:49:37 +0100

    create index & search for users

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UserController extends Controller
 {
@@ -12,38 +13,33 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        //we need paginate for infinity scroll
+        $users = User::orderBy('first_name','ASC')
+        ->select('first_name','last_name','id')
+        ->get();
+         return response()->json($users);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function search(Request $request)
     {
-        //
+        //validate that  $request->query('search') is not empty
+
+         //paginate
+         $limit = $request->query('limit', 10); // number of records per page
+         $page = $request->query('page', 1);
+         $offset = ($page - 1) * $limit; // calculate offset
+
+        $user = User::where('first_name', 'like', "%{$request->query('search')}%")
+        ->orWhere('last_name', 'like', "%{$request->query('search')}%")
+        ->orderBy('id', 'DESC')
+        ->offset($offset)
+        ->limit($limit)
+        ->get();
+
+        return response()->json($user);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, User $user)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
-    {
-        //
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\TaskController;
+use App\Http\Controllers\API\UserController;
 
 Route::get('/profile', function (Request $request) {
     return $request->user();
@@ -13,6 +14,11 @@ Route::prefix('tasks')->controller(TaskController::class)->group(function() {
     Route::post('/store', 'store')->name('tasks.create');
     Route::get('/index', 'index')->name('tasks.index');
     Route::get('/show/{id}', 'show')->name('tasks.show');
+});
+
+Route::prefix('users')->controller(UserController::class)->group(function() {
+    Route::get('/index', 'index')->name('users.index');
+    Route::get('/search', 'search')->name('users.search');
 });
 
 Route::controller(AuthController::class)->group(function () {
